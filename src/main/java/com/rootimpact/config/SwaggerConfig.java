6290af4c78f04ca8cc26e75d3aefc8ebd79f3922
eoblue <eoblue22@naver.com>
2025-02-11 20:05:32 +0900

    🎉 프로젝트 시작

---
+++ src/main/java/com/rootimpact/config/SwaggerConfig.java
@@ -0,0 +1,35 @@
+package com.rootimpact.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI OpenAPI() {
+        Info info = new Info()
+                .title("Bloom")
+                .version("1.0")
+                .description("Bloom");
+
+        String jwtSchemeName = "jwtAuth";
+
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
+
+        Components components = new Components()
+                .addSecuritySchemes(jwtSchemeName, new SecurityScheme().name(jwtSchemeName)
+                        .type(SecurityScheme.Type.HTTP)
+                        .scheme("Bearer").bearerFormat("JWT"));
+
+        return new OpenAPI().components(new Components())
+                .info(info)
+                .addSecurityItem(securityRequirement)
+                .components(components);
+    }
+}
