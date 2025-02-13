e4ce7a3f6ac49cc327c1e6ef903fad6995d26721
eoblue <eoblue22@naver.com>
2025-02-13 16:45:41 +0900

    🐛 Cors url 변경
    .

--- src/main/java/com/rootimpact/config/WebMvcConfiguration.java
+++ src/main/java/com/rootimpact/config/WebMvcConfiguration.java
@@ -14,7 +14,7 @@ public class WebMvcConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://bloom-frontend-new.vercel.app/", "http://localhost:3000")  // 특정 domain 허용.
+                        .allowedOrigins("https://bloom-frontend-new.vercel.app", "http://localhost:3000")  // 특정 domain 허용.
                         .allowedHeaders("*")  // 모든 Header 허용
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH") // 허용할 HTTP 메서드
                         .exposedHeaders("Authorization", "RefreshToken") // 클라이언트에서 접근할 수 있도록 노출할 헤더
