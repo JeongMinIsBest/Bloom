194733cb045755a632d11a2820ba51dbf5a65181
eoblue <eoblue22@naver.com>
2025-02-14 00:11:01 +0900

    🐛 Cors 에러해결
    .

--- src/main/java/com/rootimpact/config/WebMvcConfiguration.java
+++ src/main/java/com/rootimpact/config/WebMvcConfiguration.java
@@ -14,10 +14,9 @@ public class WebMvcConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://bloom-frontend-new.vercel.app", "http://localhost:3000")  // 특정 domain 허용.
-                        .allowedHeaders("*")  // 모든 Header 허용
+                        .allowedOrigins("https://bloom-frontend-new.vercel.app", "https://localhost:3000", "http://bloom-frontend-new.vercel.app", "http://localhost:3000")  // 특정 domain 허용.
+                        .allowedHeaders("Content-Type", "Authorization", "X-Custom-Header")  // 모든 Header 허용
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH") // 허용할 HTTP 메서드
-                        .exposedHeaders("Authorization", "RefreshToken") // 클라이언트에서 접근할 수 있도록 노출할 헤더
                         .allowCredentials(true); // 자격 증명 허용 (쿠키 포함 요청 가능)
             }
         };
