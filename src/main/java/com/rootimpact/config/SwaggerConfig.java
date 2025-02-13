20f1c2e3d6d8ca258b13632202296bb77bd22703
eoblue <eoblue22@naver.com>
2025-02-13 19:52:14 +0900

    🐛 HTTP Swagger Redirection to HTTPS
    .

--- src/main/java/com/rootimpact/config/SwaggerConfig.java
+++ src/main/java/com/rootimpact/config/SwaggerConfig.java
@@ -1,14 +1,22 @@
 package com.rootimpact.config;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+@OpenAPIDefinition(
+        servers = {
+                @Server(url = "https://13.125.19.104", description = "bloom https 서버입니다."),
+                @Server(url = "http://13.125.19.104", description = "bloom http 서버입니다."),
+                @Server(url = "http://localhost:8080", description = "bloom local 서버입니다.")
+        }
+)
 @Configuration
 public class SwaggerConfig {
 
@@ -27,16 +35,10 @@ public class SwaggerConfig {
                 .addSecuritySchemes(jwtSchemeName, new SecurityScheme().name(jwtSchemeName)
                         .type(SecurityScheme.Type.HTTP)
                         .scheme("Bearer").bearerFormat("JWT"));
-        // 안녕안녕
-        // 서버 설정을 추가하여 https로 호출하도록 지정
-        Server server = new Server()
-                .url("https://13.125.19.104/api") // 실제 도메인과 경로로 변경
-                .description("Production server");
 
         return new OpenAPI()
                 .components(components)
                 .info(info)
-                .addSecurityItem(securityRequirement)
-                .addServersItem(server); // servers에 https 설정 추가
+                .addSecurityItem(securityRequirement);
     }
 }
