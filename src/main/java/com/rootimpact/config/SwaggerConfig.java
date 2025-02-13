ed60d7d96081845f69f0e9b1a3e5522ad92590de
eoblue <eoblue22@naver.com>
2025-02-13 19:40:53 +0900

    ✏️ Https 설정
    .

--- src/main/java/com/rootimpact/config/SwaggerConfig.java
+++ src/main/java/com/rootimpact/config/SwaggerConfig.java
@@ -5,6 +5,7 @@ import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.security.SecurityRequirement;
 import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,6 +27,11 @@ public class SwaggerConfig {
                 .addSecuritySchemes(jwtSchemeName, new SecurityScheme().name(jwtSchemeName)
                         .type(SecurityScheme.Type.HTTP)
                         .scheme("Bearer").bearerFormat("JWT"));
+
+        // 서버 설정을 추가하여 https로 호출하도록 지정
+        Server server = new Server()
+                .url("https://13.125.19.104/api") // 실제 도메인과 경로로 변경
+                .description("Production server");
 
         return new OpenAPI().components(new Components())
                 .info(info)
