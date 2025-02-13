78ddf75d7eb8c6aa8c583107e367d329df0cd8d0
eoblue <eoblue22@naver.com>
2025-02-14 03:19:10 +0900

    ♻️ Cors
    .

--- src/main/java/com/rootimpact/config/WebMvcConfiguration.java
+++ src/main/java/com/rootimpact/config/WebMvcConfiguration.java
@@ -14,7 +14,7 @@ public class WebMvcConfiguration {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("https://bloom-frontend-new.vercel.app", "https://localhost:3000", "http://bloom-frontend-new.vercel.app", "http://localhost:3000")  // 특정 domain 허용.
+                        .allowedOrigins("https://bloom-frontend-new.vercel.app", "https://localhost:3000", "http://bloom-frontend-new.vercel.app", "http://localhost:3000", "https://bloom3.site")  // 특정 domain 허용.
                         .allowedHeaders("Content-Type", "Authorization", "X-Custom-Header")  // 모든 Header 허용
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "HEAD", "OPTIONS", "PATCH") // 허용할 HTTP 메서드
                         .allowCredentials(true); // 자격 증명 허용 (쿠키 포함 요청 가능)
